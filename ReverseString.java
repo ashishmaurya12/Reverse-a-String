610a175dc17089b22c19d9c4d67efbfeb04f7029
Ashish Maurya <123163255+ashishmaurya12@users.noreply.github.com>
2025-11-25 20:08:43 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,20 +1,21 @@
 package com.ReverseString;
 
 public class ReverseString {
-    static String reverse(String s, int left, int right) {
-        if (left >= right)
+    static String reverse(String s, int l, int r) {
+        if(l>=r)
             return s;
-        char[] arr = s.toCharArray();
-        char temp = arr[left];
-        arr[left] = arr[right];
+        char[] arr=s.toCharArray();
+        char temp =arr[l];
+        arr[l] = arr[r];
         arr[right] = temp;
-        s = new String(arr);
-        return reverse(s, left + 1, right - 1);
+        s=new String(arr);
+        return reverse(s, l + 1, r - 1);
     }
     public static void main(String[] args) {
-        String str = "abcabdae";
+        String str="abcabdae";
         String reversed = reverse(str, 0, str.length() - 1);
         System.out.println("Original: " + str);
         System.out.println("Reversed: " + reversed);
     }
 }
+
